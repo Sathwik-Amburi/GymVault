23b426cb643a5063848086c21f20b5d6ec9a5998
Raffaele Tranquillini <raffaele.tranquillini@gmail.com>
2022-06-09 16:55:28 +0200

    Update index.tsx with font awesome import conflict

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,10 @@ import axios from "axios";
 import { API_URL } from "./config/config";
 import App from "./App";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+<<<<<<< HEAD
+=======
+
+>>>>>>> e583a49 (Update index.tsx with font awesome import conflict)
 
 axios.defaults.baseURL = API_URL;
 
