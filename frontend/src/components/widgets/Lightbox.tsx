3b4de7c5684244df2a3282672c45e7f033943cb6
Raffaele Tranquillini <raffaele.tranquillini@gmail.com>
2022-06-09 16:20:02 +0200

    frontend: Make Lightbox logically interactive

--- frontend/src/components/widgets/Lightbox.tsx
+++ frontend/src/components/widgets/Lightbox.tsx
@@ -6,22 +6,24 @@ type Props = {
 }
 
 const Lightbox: FC = () => {
+    let [viewing, setViewing] = useState("#ddd");
+
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
-                <Paper style={{ padding: "10em", textAlign: "center", backgroundColor: "#ddd" }}>
+                <Paper style={{ padding: "10em", textAlign: "center", backgroundColor: viewing }}>
                 Image Lightbox Goes Here
                 </Paper>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={4} onClick={() => { setViewing("#5cc") }}>
                 <Paper style={{ padding: "3em", textAlign: "center", backgroundColor: "#5cc" }}>
                 </Paper>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={4} onClick={() => { setViewing("#9cc") }}>
                 <Paper style={{ padding: "3em", textAlign: "center", backgroundColor: "#9cc" }}>
                 </Paper>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={4} onClick={() => { setViewing("#fcc") }}>
                 <Paper style={{ padding: "3em", textAlign: "center", backgroundColor: "#fcc" }}>
                 </Paper>
             </Grid>
