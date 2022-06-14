e209e0c7037bd48f16638017a0890a7561c5d9a2
ge37zat <mohamedyehia1998@gmail.com>
2022-06-14 20:07:56 +0200

    feat: fix courses endpoints

--- backend/routes/routers/courseRouter.js
+++ backend/routes/routers/courseRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const courseController = require("../../controllers/courseController");
 
 // TODO: Add remaining endpoints
-router.get("/get-all-gyms", courseController.getAllCourses);
-router.post("/add-gym", courseController.addCourse);
+router.get("/get-all-courses", courseController.getAllCourses);
+router.post("/add-course", courseController.addCourse);
 router.get("/filter", courseController.filterCourses);
 router.get("/get/:id", courseController.getCourse);
 
