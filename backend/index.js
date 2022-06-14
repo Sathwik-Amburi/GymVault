8fb491be730ad02a366b8882c13d874146ab19ea
ge37zat <mohamedyehia1998@gmail.com>
2022-06-14 19:55:50 +0200

    feat:filter gyms backend

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 gymvaultDb.connect();
 
+app.get("/", (req, res) => {res.send("Connected to API")})
+
 app.use("/api", apiRoute);
 
 app.listen(PORT, () => {
