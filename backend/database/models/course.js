6ee312da0ee16ba25fde77413c448326704666ab
NadaBoukhari <51091307+NadaBoukhari@users.noreply.github.com>
2022-06-14 18:05:35 +0100

    Added new attributes to the schemas

--- backend/database/models/course.js
+++ backend/database/models/course.js
@@ -2,42 +2,23 @@ const mongoose = require("mongoose");
 
 // TODO: Add remaining fields, Add remaining Schemas
 const CourseSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        required: true,
-
-    },
-
-    gymId: {
-        type: String,
-        required: true,
-    },
-
-    name: {
-        type: String,
-        required: true,
-    },
-
-    description: {
-        type: String,
-        required: true,
-        default: "",
-    },
-
-    address: {
-        type: String,
-        required: true,
-        default: "",
-    },
-
-    city: {
-        type: String,
-        required: true,
-    },
-    description:  {
-        type: String,
-        required: true
-    },
+  id: {
+    type: String,
+    required: true,
+  },
+  gymId: { type: mongoose.Schema.Types.ObjectId, ref: "Gym", required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
 });
 
 const Course = mongoose.model("Course", CourseSchema);
