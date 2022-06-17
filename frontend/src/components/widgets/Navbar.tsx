ef395030653f99f40078e174d9fc47fa2f0872eb
Raffaele Tranquillini <raffaele.tranquillini@gmail.com>
2022-06-17 14:15:33 +0200

    frontend/components: Enhance navbar

--- frontend/src/components/widgets/Navbar.tsx
+++ frontend/src/components/widgets/Navbar.tsx
@@ -2,30 +2,37 @@ import { FC, useEffect, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 
-import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
+import Link from "@mui/material/Link";
+import { Avatar, Button } from "@mui/material";
 
 const Navbar: FC = () => {
+  const [user, setUser] = useState<string>("");
   return (
     <AppBar position="static" color="default" elevation={0}>
       <Toolbar sx={{ flexWrap: "wrap" }}>
         <img src="/favicon.ico" alt="Gym Sport" style={{ height: "32px" }} />
-        <Typography
+        <Link
+          href="/"
           variant="h6"
           color="inherit"
           noWrap
+          style={{ fontWeight: "bold", textDecoration: "none" }}
           sx={{ flexGrow: 1, paddingLeft: "1em" }}
         >
-          <span style={{ fontWeight: "bold", cursor: "pointer" }}>
-            GymVault
-          </span>
-        </Typography>
-        <Button color="inherit" variant="outlined" style={{ marginRight: 16 }}>
+          GymVault
+        </Link>
+        { !user && ( <Button color="inherit" variant="outlined" style={{ marginRight: 16 }}>
           Log in
-        </Button>
-        <Button color="primary" variant="outlined">
+        </Button> ) }
+        { !user && ( <Button color="primary" variant="outlined">
           Sign up
-        </Button>
+        </Button> ) }
+
+        { user && ( <Button color="inherit" variant="outlined" style={{ marginRight: 16 }}>
+          My Tickets
+        </Button> ) }
+        { user && ( <Avatar alt="User" src="/avatar.jpg" style={{ marginRight: 16 }} sx={{bgcolor: "teal" }} /> ) }
+
       </Toolbar>
     </AppBar>
   );
