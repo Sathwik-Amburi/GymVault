ea3d48939a0fefbd087be8fc71834a0f29b150b0
Raffaele Tranquillini <raffaele.tranquillini@gmail.com>
2022-06-27 16:35:58 +0200

    frontend/components: Improve confirmation

--- frontend/src/components/EmailConfirmationPage.tsx
+++ frontend/src/components/EmailConfirmationPage.tsx
@@ -1,14 +1,21 @@
+import { Typography, Paper } from "@mui/material";
 import { FC } from "react";
 import { useParams } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 const EmailConfirmationPage: FC = () => {
   const { email } = useParams();
   return (
-    <div>
-      <h1>
-        We have sent you an email at {email}. Please check your email to verify
-        your account !
-      </h1>
+    <div style={{ padding: "10%", borderRadius: "1rem", backgroundColor: "#eee" }}>
+      <Typography variant="h4">
+        <i className="fas fa-paper-plane" style={{ paddingRight: "0.5em" }} />
+        Check your Inbox!
+      </Typography>
+      <Typography variant="body1" style={{ marginTop: "1rem" }}>
+        We have sent an email to <b>{email}</b> with a link to confirm your email
+        address.
+      </Typography>
     </div>
   );
 };
