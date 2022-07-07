2f6d55dabc8c41197bab56d08b12fefe6f89b652
Raffaele Tranquillini <raffaele.tranquillini@gmail.com>
2022-07-07 18:22:30 +0200

    frontend: add 403 handler

--- frontend/src/components/widgets/utilities/UnifiedErrorHandler.tsx
+++ frontend/src/components/widgets/utilities/UnifiedErrorHandler.tsx
@@ -1,6 +1,15 @@
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
+
 export default class UnifiedErrorHandler {
     public static handle(error: Error, message: string): void {
-        alert(message);
+        if (error.name === "AxiosError" && (error as AxiosError).response!.status === 401) {
+            alert("Token expired! Please log in again");
+            localStorage.removeItem("token");
+            document.location.href = "/user/login";
+        } else {
+            alert(message);
+        }
         console.error(error);
     }
 }
