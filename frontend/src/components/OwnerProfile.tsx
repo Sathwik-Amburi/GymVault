db99ed5854039d1a3dd61526c12f36105e302a70
ge37zat <ge37zat@mytum.de>
2022-07-07 05:49:47 +0200

    stripe edit payout settings, check if user is connected

--- frontend/src/components/OwnerProfile.tsx
+++ frontend/src/components/OwnerProfile.tsx
@@ -1,17 +1,14 @@
-import { Button } from "@mui/material";
-import axios from "axios";
 import { FC, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import ApiCalls from "../api/apiCalls";
 import { UserProfileDetails } from "../models/allModels";
+import OwnerHomePage from "./OwnerHomePage";
+import StripeOnboard from "./StripeOnboard";
 import UnifiedErrorHandler from "./widgets/utilities/UnifiedErrorHandler";
 
 
 const OwnerProfile: FC = () => {
   const [profile, setProfile] = useState<UserProfileDetails>();
-  const [loading, setLoading] = useState<boolean>(false)
   const token = localStorage.getItem("token");
-  const navigate = useNavigate()
 
   useEffect(() => {
     ApiCalls.getUserProfile(token)
@@ -20,13 +17,7 @@ const OwnerProfile: FC = () => {
       }).catch((err) => UnifiedErrorHandler.handle(err, "Cannot get user profile"));
   }, []);
 
-  const handleClick = async () => {
 
-    const headers = {"x-access-token": String(localStorage.getItem('token'))}
-    setLoading(true)
-    let response = await axios.post('/stripe/connect', {}, {headers})
-    window.location.href = response.data.link
-  }
 
   return (
     <>
@@ -37,12 +28,10 @@ const OwnerProfile: FC = () => {
       <div>Phone Number: {profile?.phoneNumber}</div>
       <div>Email: {profile?.email}</div>
 
-      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", padding: "10px 10px", marginTop:"40px" }}>
-        <i style={{ margin: "20px 0", fontSize: "90px" }} className="fa-brands fa-cc-stripe" aria-hidden="true"></i>
-        <div style={{ margin: "20px 0" }}><b>Set up your payouts to be able to post your gym for everyone to enjoy!</b> </div>
-        {!loading ? <Button onClick={handleClick} variant="contained">Onboard</Button> : <Button disabled onClick={handleClick} variant="contained">Processing . . .</Button>}
-        <div style={{ fontSize: "12px", margin: "11px 3px", color:"grey" }}> <i>You will be redirected to Stripe to start the onboarding process</i> </div>
-      </div>
+      {profile?.payouts_enabled ? <OwnerHomePage/> : <StripeOnboard/>}
+
+      
+
     </>
   );
 }
